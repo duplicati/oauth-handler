5c246b076c009e743e1397c8e17d52e3f27349ad
Carl Johnsen <carl-johannes@di.ku.dk>
2024-12-13 08:14:47 +0100

    Updated password generator to python3.12

--- password_generator.py
+++ password_generator.py
@@ -1,42 +1,28 @@
-from os import urandom
-from random import choice
+import random
 
-char_set = {'small': 'abcdefghijklmnopqrstuvwxyz',
-             'nums': '0123456789',
-             'big': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-             'special': '!-_.'
-            }
-
+char_set = list(enumerate([
+    'abcdefghijklmnopqrstuvwxyz', # Lowercase
+    '0123456789', # Numbers
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ', # Uppercase
+    '!-_.' # Special characters
+]))
 
 def generate_pass(length=21):
     """Function to generate a password"""
 
     password = []
+    prev = -1
 
     while len(password) < length:
-        key = choice(char_set.keys())
-        a_char = urandom(1)
-        if a_char in char_set[key]:
-            if check_prev_char(password, char_set[key]):
-                continue
-            else:
-                password.append(a_char)
+        i, a_char_set = random.choice(char_set)
+        if i == prev:
+            continue
+        else:
+            prev = i
+            a_char = random.choice(a_char_set)
+            password.append(a_char)
+
     return ''.join(password)
 
-
-def check_prev_char(password, current_char_set):
-    """Function to ensure that there are no consecutive 
-    UPPERCASE/lowercase/numbers/special-characters."""
-
-    index = len(password)
-    if index == 0:
-        return False
-    else:
-        prev_char = password[index - 1]
-        if prev_char in current_char_set:
-            return True
-        else:
-            return False
-
 if __name__ == '__main__':
-    print generate_pass()
+    print (generate_pass())
